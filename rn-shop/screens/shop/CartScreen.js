98c612ffa12e432f1fa46c2341a374eb036794c3
Faj5097 <fab.jan@t-online.de>
2021-08-29 18:40:39 +0200

    Cart Screen

--- rn-shop/screens/shop/CartScreen.js
+++ rn-shop/screens/shop/CartScreen.js
@@ -1,11 +1,63 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+
+import Colors from "../../constants/Colors";
 
 const CartScreen = (props) => {
-    return <View>
-        <Text>Total Amount: <Text>$ {19.99}</Text></Text>
-        <Text>Items</Text>
+    const cartTotalAmount = useSelector(state => state.cart.totalAmount);
+    const cartItems = useSelector(state => {
+        const transformedCartItems = [];
+        
+        for (const key in state.cart.items) {
+            transformedCartItems.push({
+                productId: key,
+                productPrice: state.cart.items[key].prodPrice,
+                productTitle: state.cart.items[key].prodTitle,
+                quantity: state.cart.items[key].quantity,
+                sum: state.cart.items[key].sum
+            })
+        }
+
+        return transformedCartItems;
+    })
+    
+    return <View style={styles.screen}>
+    <View style={styles.summary}>
+        <Text style={styles.summaryText}>Total: <Text style={styles.amount}>$ {cartTotalAmount.toFixed(2)}</Text></Text>
+        <Button color={Colors.accent} title="Order Now" disabled={cartItems.length === 0}/>
+    </View>
+    <View>
+        <Text>Cart Items</Text>
+    </View>
     </View>
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        margin: 20
+    },
+    summary: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: 20,
+        padding: 10,
+        shadowColor: "black",
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        elevation: 5,
+        borderRadius: 10,
+        backgroundColor: "white"
+    },
+    summaryText: {
+        fontFamily: "open-sans-bold",
+        fontSize: 18
+    },
+    amount: {
+        color: Colors.primary
+    }
+});
 
 export default CartScreen;
