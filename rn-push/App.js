386b0904a4e87e8988a8ca7622035b9b8685b9ca
Faj5097 <fab.jan@t-online.de>
2021-09-17 23:00:26 +0200

    Local Notifications

--- rn-push/App.js
+++ rn-push/App.js
@@ -1,11 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Button, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: false
+    };
+  }
+});
 
 export default function App() {
+  useEffect(() => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS)
+      .then((statusObj) => {
+        if (statusObj.status !== "granted") {
+          return Permissions.askAsync(Permissions.NOTIFICATIONS);
+        }
+        return statusObj;
+      })
+      .then((statusObj) => {
+        if (statusObj.status !== "granted") {
+          return;
+        }
+      });
+  }, []);
+
+  const triggerNotificationHandler = () => {
+    //Schedule a local Notification
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local notification!",
+        body: "This is the first local notification we are sending!"
+      },
+      trigger: {
+        seconds: 10
+      }
+    });
+  };
+
+  useEffect(() => {
+    const backgroundSubscription =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    const foregroundSubscription =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+
+    return () => {
+      backgroundSubscription.remove();
+      foregroundSubscription.remove();
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Push Notification" onPress={triggerNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +70,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
